9fbfe5106f321bea7183c0edddc408b9a7560a64
EvanCharles16 <evancharles1609@gmail.com>
2020-05-28 03:22:49 +0700

    decrease image size

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -141,6 +141,124 @@ const Portfolio = () => {
 							</Card>
 						</Col>
 					</Row>
+
+					{/* <Row>
+						<Col md={4} sm={12}>
+							<Card className="portfolioCard mx-auto">
+								<Card.Img variant="top" src={designx} />
+
+								<Card.Body>
+									<Button
+										onClick={() => setOpen4(!open)}
+										aria-controls="example-collapse-text"
+										aria-expanded={open4}
+										block
+									>
+										Click for Details
+										<i class="fas fa-caret-down ml-2 detailButton"></i>
+									</Button>
+
+									<Collapse in={open4}>
+										<div id="example-collapse-text">
+											<Card.Body>
+												<Card.Title>DesignX #1</Card.Title>
+												<Card.Text>A Simple Responsive Web</Card.Text>
+
+												<Card.Title>Technologies</Card.Title>
+												<Card.Text>Pure HTML CSS</Card.Text>
+												<a
+													href="https://designx.netlify.app"
+													target="_blank"
+													className="btn btn-primary btn-block"
+												>
+													Visit Website
+													<i class="fas fa-arrow-circle-right ml-2"></i>
+												</a>
+											</Card.Body>
+										</div>
+									</Collapse>
+								</Card.Body>
+							</Card>
+						</Col>
+
+						<Col md={4} sm={12}>
+							<Card className="portfolioCard mx-auto">
+								<Card.Img variant="top" src={hybeast} />
+
+								<Card.Body>
+									<Button
+										onClick={() => setOpen5(!open5)}
+										aria-controls="example-collapse-text"
+										aria-expanded={open5}
+										block
+									>
+										Click for Details
+										<i class="fas fa-caret-down ml-2 detailButton"></i>
+									</Button>
+
+									<Collapse in={open5}>
+										<div id="example-collapse-text">
+											<Card.Body>
+												<Card.Title>Hybeast #2</Card.Title>
+												<Card.Text>A Simple Responsive Web</Card.Text>
+
+												<Card.Title>Technologies</Card.Title>
+												<Card.Text>HTML CSS, SASS, Bootstrap</Card.Text>
+												<a
+													href="https://hybeastz.netlify.app"
+													target="_blank"
+													className="btn btn-primary btn-block"
+												>
+													Visit Website
+													<i class="fas fa-arrow-circle-right ml-2"></i>
+												</a>
+											</Card.Body>
+										</div>
+									</Collapse>
+								</Card.Body>
+							</Card>
+						</Col>
+
+						<Col md={4} sm={12}>
+							<Card className="portfolioCard mx-auto">
+								<Card.Img variant="top" src={reactweb} />
+
+								<Card.Body>
+									<Button
+										onClick={() => setOpen6(!open6)}
+										aria-controls="example-collapse-text"
+										aria-expanded={open6}
+										block
+									>
+										Click for Details
+										<i class="fas fa-caret-down ml-2 detailButton"></i>
+									</Button>
+
+									<Collapse in={open6}>
+										<div id="example-collapse-text">
+											<Card.Body>
+												<Card.Title>Personal Website</Card.Title>
+												<Card.Text>A Simple Personal Web</Card.Text>
+
+												<Card.Title>Technologies</Card.Title>
+												<Card.Text>
+													ReactJS, Bootstrap, React Bootstrap
+												</Card.Text>
+												<a
+													href="https://vannch.com"
+													target="_blank"
+													className="btn btn-primary btn-block"
+												>
+													Visit Website
+													<i class="fas fa-arrow-circle-right ml-2"></i>
+												</a>
+											</Card.Body>
+										</div>
+									</Collapse>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row> */}
 				</div>
 			</Container>
 			<Socialmedia />
